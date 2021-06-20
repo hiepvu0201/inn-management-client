0f38abc72f971a4745cd45064c33a980d3d5cd5a
hiep.vu.0201@gmail.com <hiep.vu.0201@gmail.com>
2021-06-21 06:18:02 +0700

    change http to https

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 const axiosClient = axios.create({
   // baseURL: process.env.REACT_APP_API_BASE_URL,
   // baseURL: "http://localhost:8080",
-  baseURL: "http://inn-server.herokuapp.com/",
+  baseURL: "https://inn-server.herokuapp.com/",
   headers: {
     "Content-Type": "application/json",
     "Access-Control-Allow-Origin": "*",
